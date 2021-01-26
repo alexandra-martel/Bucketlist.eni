c7975c7b6d652563e4314f8b662b854c1bb6cd8f
Alexandra <alexandra.martel2020@campus-eni.fr>
2021-01-26 14:59:06 +0100

    Tp 3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 Class MainController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="main_home", methods={"GET"})
      */
 
-    public function home()
+    public function home(): Response
     {
-        return $this->render('main/home.html.twig');
+        return $this->render('main/home.html.twig', []);
     }
 }
